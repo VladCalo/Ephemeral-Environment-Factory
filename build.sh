bb6c9b23dc429473283ec1ccc399a9d540e0b253
VladCalo <80626362+VladCalo@users.noreply.github.com>
2025-07-02 20:04:57 +0300

    Check argoCD working, change whoami replica count

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@
 # ansible main playbook
 # export KUBECONFIG=$(pwd)/admin.conf
 # argoCD playbook
-# port-forward argoCD
+# port-forward argoCD: kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+
+# get password
+# kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d && echo
